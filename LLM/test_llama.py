2db995096760815803444f801d613a57eb69c6f9
Emilia <emilia.a.wis@gmail.com>
2024-03-28 10:35:32 +0100

    dzialajace uruchamianie ollamy

--- LLM/test_llama.py
+++ LLM/test_llama.py
@@ -1,16 +1,14 @@
 # KOD WZIĘTY Z: https://github.com/ollama/ollama/blob/main/examples/python-simplechat/client.py
 # Zmodyfikowany przez: Emilia Wiśniewska
 
-# jeszcze nie wiem, czy dziala bo mi na windowsie nie chce dzialac
-
 import json
 import requests # powinnno działać, jeśli nie to: pip install requests
 
-# NOTE: ollama must be running for this to work, start the ollama app or run `ollama serve`
-model = "llama2"  # TODO: update this for whatever model you wish to use
+# NOTE: żeby działało musi być uruchomiona ollama ("$ ollama serve" lub uruchomiona aplikacja)
+model = "llama2"
 
 
-def chat(messages):
+def answer(messages):
     r = requests.post(
         "http://0.0.0.0:11434/api/chat",
         json={"model": model, "messages": messages, "stream": False},
@@ -20,19 +18,12 @@ def chat(messages):
     body = json.loads(r.text)
     message = body.get("message", "")
     content = message.get("content", "")
-    # output = content
-    print(content)
-    return message
+    return content
 
-def main():
-    messages = []
 
-    while True:
-        user_input = input("Enter a prompt: ")
-        if not user_input:
-            exit()
-        print()
-        messages.append({"role": "user", "content": user_input})
-        message = chat(messages)
-        messages.append(message)
-        print("\n\n")
+while True:
+    user_input = input("Enter a prompt: ")
+    if not user_input:
+        exit()
+    print("llama2:", answer([{"role": "user", "content": user_input}]))
+    print("\n")
