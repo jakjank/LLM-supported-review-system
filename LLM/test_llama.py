3ea1debdf10ea766af6be8c45f03529b41cb75ae
Emilia <emilia.a.wis@gmail.com>
2024-04-04 22:17:26 +0200

    dodanie sensownego jakkolwiek testu do test_llama.py

--- LLM/test_llama.py
+++ LLM/test_llama.py
@@ -3,6 +3,7 @@
 
 import json
 import requests # powinnno działać, jeśli nie to: pip install requests
+import pytest
 
 # NOTE: żeby działało musi być uruchomiona ollama ("$ ollama serve" lub uruchomiona aplikacja)
 model = "llama2"
@@ -21,9 +22,21 @@ def answer(messages):
     return content
 
 
-while True:
-    user_input = input("\033[1;92m>>> \033[0;37m")
-    if not user_input:
-        exit()
-    print("\033[1;92mllama2:\033[0;37m", answer([{"role": "user", "content": user_input}]))
-    print("\n")
+def post_question(user_input):
+	question = [{"role": "user", "content": user_input}]
+	return answer(question)
+
+def main():
+	while True:
+		user_input = input("\033[1;92m>>> \033[0;37m")
+		if not user_input:
+			exit()
+		print("\033[1;92mllama2:\033[0;37m", post_question(user_input))
+		print("\n")
+
+if __name__ == "__main__":
+	main()
+
+def test_answer():
+	assert(post_question("hi!") != "")		
+
--- Research/pytest_info.md
+++ Research/pytest_info.md
@@ -28,8 +28,8 @@ a jeśli nie, to:
 
 ## Dodatkowe info:
 
-1. pytest sprawdza wszystkie pliki test_*.py i *_test.py, dlatego jak uruchomimy w /LLM_... to będzie próbować uruchamiać test_llama.py, dlatego będzie trzeba zmienić pewnie później nazwę pliku
-2. można jeszcze jakieś fajne rzeczy z przechwytywaniem errorów czy innych robić, potem dorzucę do kodu przykłady
+1. pytest sprawdza wszystkie pliki test_*.py i *_test.py, dlatego uruchamia test_sample.py oraz test_llama.py
+2. można jeszcze jakieś fajne rzeczy z przechwytywaniem errorów czy innych robić, potem dorzucę do kodu przykłady(na razie jest coś co działa i nawet sprawdza coś sensownego)
 3. dokumentacja jest fajna i czytelna, więc w razie pytań polecam
+4. Na ten moment jest sprawdzanie, czy dostaniemy niepustą odpowiedź od ollamy na zapytanie 'hi!'
 
-to be updated (prawdopodobnie)...
