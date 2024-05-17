b546e18ab73d3db6f5692f1ec3ee770917237c63
Emilia <emilia.a.wis@gmail.com>
2024-05-17 21:33:11 +0200

    Asana-37-ustruturyzowanie-odpowiedzi-llamy-part-1
    Dokonane zmiany:
    1. usuniecie zapytania o niepotrzebny kod
    2. wyczyszczenie i poprawienie kodu llama_sample oraz prompt
    3. zmuszenie (tak jak sie da) llamy do zwrocenia jsona ze zmiennymi
    4. jesli to zrobi wykorzystanie tego (wyjecie z niego zmiennych)
    5. dodanie opcji debugu
    6. zmuszenie (tak jak sie da) llamy do stworzenia przykladowego
    zmienionego diffa

--- project/src/project/prompt1.py
+++ project/src/project/prompt1.py
@@ -1,41 +1,79 @@
 from llama_sample import post_question
+import json
+import click
 
 
-def check_file(filename, ansname=None):
+def check_file(filename, ansname=None, context=None, debug=False):
 	"""
 	function uses ollama llama2 model to check if in given
 	diff from pull request should some variable names be changed
 	it also checks for unnecessary fragments of code
 	:param filename: name of file with diff from pull request
+	:param ansname: name of file where answer should be saved
+	:param context: name of file with context of diff
 	:return: answer from ollama how (and if) to change pull request is
+	:debug: if True, print answer instead of saving it in a "ansname" file
 	saved in a file
 	"""
+
+
 	with open(filename, "r") as data:
-		variables = post_question("Return all variable names from this diff"
-								  "from pull-request" + data.read())
-		variables_to_change = post_question(f"Check if all variable names ({variables}) "
-											f"are correct and"
-							" will not create problems later. "
-							"\nIs there something that should be changed?")
+		with open(context, "r") as context_fd:
+			variables = post_question(f""" Please return all variable names from
+							 this diff from pull-request:
+							  {data.read()}.
+							This is the context of the diff: {context_fd.read()}.
+							Please use json format to return the answer. All variables 
+							 should be saved in the "variables" key. Any extra comment 
+							 on variables or other things
+							 should be provided using the
+							 "comment" key. Please do not write anything else
+							 that the json object, its really important.
+								  """)	
+			# making json object from string if possible:
+			try:
+				variables = json.loads(variables)
+				variables = variables["variables"]
+			except json.JSONDecodeError or KeyError:
+				if debug:
+					print("Error in json format")
 
-		unnecessary_code = post_question("Check if in that diff"
-								"are some unnecessary fragments"
-								"of code that should be replaced or can"
-								"be just removed")
-		if ansname is None:
-			return("Zmienne:\n"+ str(variables) + "\n\n Komentarz:" +
-				variables_to_change +"\n\n\n\n" + unnecessary_code)
-		else:
-			with open(ansname, "w") as ans:
-				ans.write(variables_to_change +"\n\n\n\n" + unnecessary_code)
 
-import click
+			variables_to_change = post_question(f"""Check if all variable names from
+								{variables} are correct and will not create problems later
+								in the code. If not, suggest some new variable names.
+								The diff file is: {data.read()}
+								The context of the diff is: {context_fd.read()}
+								If the variable names are not correct, please provide
+								a new diff with correct variable names as well as
+								a comment on why the variable names should be changed below.""")
+
+			# unnecessary_code = post_question("Check if in that diff"
+			# 					"are some unnecessary fragments"
+			# 					"of code that should be replaced or can"
+			# 					"be just removed")
+
+			if debug:
+				print(variables)
+				print("\n\n\n\n")
+				print(variables_to_change)
+			else:
+				with open(ansname, "w") as ans:
+						ans.write(variables_to_change)
+
+
+
 @click.command()
 @click.option('--input', default="przykladowy_diff.txt", help='Name of the file with ollama input')
 @click.option('--output', default="ollama_output.txt", help='Name of the file for ollama output')
-def main(input, output):
-    check_file(input, output)
+@click.option('--context', default="context.txt", help='Name of the file with context of diff')
+@click.option('--debug', default=False, help='Debug mode')
+def main(input, output, context, debug):
+    check_file(input, output, context, debug)
 
 
 if __name__ == "__main__":
 	main()
+
+
+# rozbic na dwa prompty, zapytac o bardziej skomplikowane rzeczy, np jak uprościć kod
